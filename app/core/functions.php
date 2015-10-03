de2ed0a4b990c2b625fe17f007e176053de795d2
Jiří Bělský <jiribelsky@hotmail.com>
2015-10-04 01:34:09 +0200

    Admin login tweaked; Added PHP get_browser() function to get browser info

--- app/core/functions.php
+++ app/core/functions.php
@@ -11,18 +11,6 @@ class core {
 			echo '<div class="sys_message" style="font-size:11px;background:#f2f2f2;padding:3px 5px;border:1px solid #f2f2f2;color:black;border-radius:2px;margin:2px;font-family:arial;">'.$message.'</div>';
 	}
 
-	/*public function destroy_session(){
-		$_SESSION = 0;
-		if (ini_get("session.use_cookies")) {
-		    $params = session_get_cookie_params();
-		    setcookie(session_name(), '', time() - 42000,
-		        $params["path"], $params["domain"],
-		        $params["secure"], $params["httponly"]
-		    );
-		}
-		session_destroy();
-	}*/
-
 	public static function get($i){
 		if(isset($_GET[$i])){
 			return htmlspecialchars($_GET[$i]);
@@ -127,112 +115,6 @@ class core {
          	return $ram;
 	}
 	
-	
-	public static function get_browser() { 
-		$u_agent = $_SERVER['HTTP_USER_AGENT']; 
-		$bname = 'Unknown';
-		$platform = 'Unknown';
-		$version= "";
-		
-		//First get the platform?
-		if (preg_match('/linux/i', $u_agent)) {
-			$platform = 'linux';
-		}
-		elseif (preg_match('/macintosh|mac os x/i', $u_agent)) {
-			$platform = 'mac';
-		}
-		elseif (preg_match('/windows|win32/i', $u_agent)) {
-			$platform = 'windows';
-		}
-		
-		// Next get the name of the useragent yes seperately and for good reason
-		if(preg_match('/MSIE/i',$u_agent) && !preg_match('/Opera/i',$u_agent)) 
-		{ 
-			$bname = 'Internet Explorer'; 
-			$ub = "MSIE"; 
-		} 
-		elseif(preg_match('/Firefox/i',$u_agent)) 
-		{ 
-			$bname = 'Mozilla Firefox'; 
-			$ub = "Firefox"; 
-		} 
-		elseif(preg_match('/Chrome/i',$u_agent)) 
-		{ 
-			$bname = 'Google Chrome'; 
-			$ub = "Chrome"; 
-		} 
-		elseif(preg_match('/Safari/i',$u_agent)) 
-		{ 
-			$bname = 'Apple Safari'; 
-			$ub = "Safari"; 
-		} 
-		elseif(preg_match('/Opera/i',$u_agent)) 
-		{ 
-			$bname = 'Opera'; 
-			$ub = "Opera"; 
-		} 
-		elseif(preg_match('/Netscape/i',$u_agent)) 
-		{ 
-			$bname = 'Netscape'; 
-			$ub = "Netscape"; 
-		} 
-		
-		// finally get the correct version number
-		$known = array('Version', $ub, 'other');
-		$pattern = '#(?<browser>' . join('|', $known) .
-		')[/ ]+(?<version>[0-9.|a-zA-Z.]*)#';
-		if (!preg_match_all($pattern, $u_agent, $matches)) {
-			// we have no matching number just continue
-		}
-		
-		// see how many we have
-		$i = count($matches['browser']);
-		if ($i != 1) {
-			//we will have two since we are not using 'other' argument yet
-			//see if version is before or after the name
-			if (strripos($u_agent,"Version") < strripos($u_agent,$ub)){
-				$version= $matches['version'][0];
-			}
-			else {
-				$version= $matches['version'][1];
-			}
-		}
-		else {
-			$version= $matches['version'][0];
-		}
-		
-		// check if we have a number
-		if ($version==null || $version=="") {$version="?";}
-		
-		return array(
-			'userAgent' => $u_agent,
-			'name'      => $bname,
-			'version'   => $version,
-			'platform'  => $platform,
-			'pattern'    => $pattern
-		);
-	} 
-
-/*
-
-	PROBABLY OBSOLETE
-
-	public function count_files($path) {
-	    $size = 0;
-	    $ignore = array('.','..','cgi-bin','.DS_Store');
-	    $files = scandir($path);
-	    foreach($files as $t) {
-	        if(in_array($t, $ignore)) continue;
-	        if (is_dir(rtrim($path, '/') . '/' . $t)) {
-	            $size += self::count_files(rtrim($path, '/') . '/' . $t);
-	        } else {
-	            $size++;
-	        }   
-	    }
-	    return $size;
-	}
-
-*/
 
 }
 
--- app/modules/admin/login/login.php
+++ app/modules/admin/login/login.php
@@ -1,20 +1,15 @@
 <?php
 
-
-
 class login extends module {
-
-
 	public function createhash($input,$timestamp){
 		$salt = sha1($timestamp);
 		$hash = sha1($input . $salt);
 		return $hash;
 	}
-	
+
 	public function checklogin(){
 		$session = session_id();
 		$ip = $_SERVER["REMOTE_ADDR"];
-
 		$check = dibi::query('SELECT * FROM admins_logged WHERE logged_sessionid=%s',$session,' AND logged_ip=%s',$ip,' LIMIT 1');
 		$status = $check->fetchPairs('loggedID','logged_adminID');
 		if($status)
@@ -22,31 +17,24 @@ class login extends module {
 		else 
 			return false;
 	}
-	
+
 	public function admin_login(){
 		$session = session_id();
 		$admin_email = core::post("admin_email");
 		$admin_password = core::post("admin_password");
-		
-		// NEED TIMESTAMP FROM USER TO HASH PASSWORD FROM FORM
+
+		// CHECK IF USER EXISTS - RETURNS TIMESTAMP TO HASH PASSWORD FOR COMPLETE USER CHECK
 		$check_user = dibi::query('SELECT * FROM admins WHERE admin_email=%s',$admin_email,' LIMIT 1');
 		$user = $check_user->fetch("admin_created");
-		
-		//echo $this->createhash("admin","2015-10-04 00:28:30");
-		/*echo "<pre>";
-		var_dump($user["admin_created"]);
-		echo "</pre>";*/
-		
 		$admin_password = $this->createhash($admin_password,$user["admin_created"]);
-		echo $admin_password;
-		
+
 		// USER EXISTS - LETS CHECK PASSWORD
 		if($user){
 			$check_pass = dibi::query('SELECT * FROM admins WHERE admin_email=%s',$admin_email,' AND admin_password=%s',$admin_password,'');
 			$admin = $check_pass->fetch("adminID");
 			if($admin){
-				$browser = core::get_browser();
-				$browser = $browser["name"] ." ". $browser["version"];
+				$browser = get_browser(null, true);
+				$browser = $browser["parent"];
 				$data = array(
 					"logged_sessionid" => $session,
 					"logged_ip" => $_SERVER["REMOTE_ADDR"], // DEV ONLY
@@ -57,119 +45,26 @@ class login extends module {
 				);
 				
 				$create = dibi::query("INSERT INTO admins_logged",$data);
-				if($create){
+				if($create)
 					header("location: /admin/login_complete=1");
-				}
-				else {
+				else 
 					header("location: /admin/login_failed=1");
-				}
 			}
-			else {
+			else 
 				header("location: /admin/login_failed=1");
-			}
 		}
-		else {
+		else 
 			header("location: /admin/login_failed=1");
-		}
 	}
 
 	public function admin_logout(){
 		$session = session_id();
 		$logout = dibi::query('DELETE FROM admins_logged WHERE logged_sessionid=%s',$session,'');
-		if($logout){
+		if($logout)
 			header("location: /admin/logout_complete=1");
-		}
-		else {
+		else 
 			header("location: /admin/logout_failed=1");
-		}
-		
 	}
-
-/*
-	public function checklogin(){
-		
-		if($this->core->getget("admin")=="1"){
-			if($this->core->getpost('action')=="login"){
-				$this->core->d_mess("PŘIHLAŠUJI - vkládám funkci na kontrolu z DB a vložení session");
-				$this->login();
-				header("location: /admin/");
-			}
-			elseif($this->core->Getget('action')=="logout"){
-				$this->core->d_mess("ODHLAŠUJI - mažu session");
-			 	$this->logout();
-				header("location: /admin/");
-			}
-		}
-		
-		$user = htmlspecialchars($this->core->getsession('admin_user'));
-		$pass = $this->core->getsession('admin_password');
-
-		if($user AND $pass){
-			$user_check = dibi::query('SELECT * FROM users_admin WHERE admin_public=1 AND %and', array(
-				array('admin_name		= %s', $user),
-				array('admin_password	= %s', $pass),
-			),' LIMIT 1');
-			$check = $user_check->fetchPairs('admin_name', 'admin_password');
-
-			if($check){
-				return true;
-			}
-			else
-				return false;
-		}
-		else
-			return false;
-		
-	}
-
-	public function login(){
-		$user = htmlspecialchars($this->core->GetPOST('username'));
-		$pass = $this->core->GetPOST('password');
-		$pass =  sha1($pass . $this->config['db_pswsalt']);
-
-		if($user AND $pass){
-			$user_check = dibi::query('SELECT * FROM users_admin WHERE %and', array(
-				array('admin_name		= %s', $user),
-				array('admin_password	= %s', $pass),
-			),' LIMIT 1');
-			$check = $user_check->fetchPairs('admin_name', 'admin_password');
-			if($check){
-				foreach($user_check as $user){
-					$_SESSION['adminID']		= $user["adminID"];
-					$_SESSION['admin_user']		= $user["admin_name"];
-					$_SESSION['admin_password'] = $user["admin_password"];
-					$_SESSION['admin_real_name']= $user["admin_real_name"];
-					$_SESSION['admin_avatar']	= $user["admin_avatar"];
-					return true;
-				}
-			}
-			else
-				return false;
-		}
-		else
-			return false;
-	}
-
-	public function logout(){
-		$_SESSION['admin_user']		= null;
-		$_SESSION['admin_password'] = null;
-		$_SESSION['admin_real_name']= null;
-		session_destroy();
-	}
-*/
-/*
-	public function router(){
- 		switch($this->core->Getget('action')){
- 			case "login":
- 				return $this->login();
- 			break;
- 			case "logout":
- 				return $this->logout($_SESSION);
- 			break;
- 			default:
- 				return $this->checklogin();
- 		}
-	}*/
 }
 
 ?>
